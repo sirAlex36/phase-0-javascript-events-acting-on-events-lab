74fd6b1fc175aed2b59eb34cad17f1909789ba9b
SirAlex36 <alexngenoo@gmail.com>
2025-09-24 16:04:38 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function moveDodgerLeft(){
+  const dodger = document.getElementById("dodger");
+  const left = parseInt(dodger.style.left);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+function moveDodgerRight(){
+  const dodger = document.getElementById('dodger');
+  const left = parseInt(dodger.style.left);
+  if (left < window.innerWidth) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
